1e1429e34e9be01e6d2b2052a1ac5f2939834234
Max <mshvarts@sfu.ca>
2020-01-30 15:57:01 -0800

    undo Client.cpp commit

--- lib/networking/src/Client.cpp
+++ lib/networking/src/Client.cpp
@@ -33,9 +33,7 @@ public:
       ioService{},
       websocket{ioService} {
     boost::asio::ip::tcp::resolver resolver{ioService};
-	boost::asio::string_view boost_address{ address.data(), address.size() };
-	boost::asio::string_view boost_port{ port.data(), port.size() };
-	connect(resolver.resolve(boost_address, boost_port));
+    connect(resolver.resolve(address, port));
   }
 
   void disconnect();
