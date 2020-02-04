ec38ab9afd40efcbb445edff3f8f2ee7dac97b41
ekkozak <ekkozak@sfu.ca>
2020-02-03 17:03:25 -0800

    Refactor connection-related structs into separate include

--- lib/networking/include/Server.h
+++ lib/networking/include/Server.h
@@ -9,6 +9,8 @@
 #ifndef NETWORKING_SERVER_H
 #define NETWORKING_SERVER_H
 
+#include "Connections.h"
+
 #include <deque>
 #include <functional>
 #include <memory>
@@ -17,38 +19,6 @@
 
 
 namespace networking {
-
-
-/**
- *  An identifier for a Client connected to a Server. The ID of a Connection is
- *  guaranteed to be unique across all actively connected Client instances.
- */
-struct Connection {
-  uintptr_t id;
-
-  bool
-  operator==(Connection other) const {
-    return id == other.id;
-  }
-};
-
-
-struct ConnectionHash {
-  size_t
-  operator()(Connection c) const {
-    return std::hash<decltype(c.id)>{}(c.id);
-  }
-};
-
-
-/**
- *  A Message containing text that can be sent to or was recieved from a given
- *  Connection.
- */
-struct ConnectionMessage {
-  Connection connection;
-  std::string text;
-};
 
 
 /** A compilation firewall for the server. */
