6a14459417057469844fe425878fbfd082267f03
Max <mshvarts@sfu.ca>
2020-02-28 01:42:39 -0800

    initial googletest commit

---
+++ lib/room/test/RoomTest.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "Room.h"
+
+TEST(RoomTest, testName)
+{
+	// Check initialization
+	Room room = Room("Room1");
+	EXPECT_EQ(room.getNumOfPlayers(), 0);
+	EXPECT_EQ(room.getRoomName(), "Room1");
+
+	room.setRoomName("Room2");
+	EXPECT_EQ(room.getRoomName(), "Room2");
+}
