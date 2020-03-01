68e46bb6ae53e1a841c7f91e3ce11be1c61a4353
Max <mshvarts@sfu.ca>
2020-02-29 16:05:32 -0800

    Add more test cases and split up existing ones

--- lib/room/test/RoomTest.cpp
+++ lib/room/test/RoomTest.cpp
@@ -1,16 +1,35 @@
 #include "gtest/gtest.h"
 #include "Room.h"
+#include "User.h"
 
 TEST(RoomTest, testName)
 {
 	// Check initialization
 	Room room = Room("Room1");
-	EXPECT_EQ(room.getNumOfPlayers(), 0);
 	EXPECT_EQ(room.getRoomName(), "Room1");
 
 	room.setRoomName("Room2");
 	EXPECT_EQ(room.getRoomName(), "Room2");
 	EXPECT_NE(room.getRoomName(), "Room1");
+
 }
 
+TEST(RoomTest, testNumOfPlayers) {
 
+	Room room = Room("Room1");
+	User user = User("Bob");
+	room.addUser(user.getUserId());
+	EXPECT_EQ(room.getNumOfPlayers(), 1);
+
+	room.removeUser(user.getUserId());
+	EXPECT_EQ(room.getNumOfPlayers(), 0);
+}
+
+// TODO: find a way to retrieve username from userlist
+/*TEST(RoomTest, testUserList) {
+	Room room = Room("Room1");
+	User user = User("John");
+	room.addUser(user.getUserId());
+	cout << room.getUserList();
+	EXPECT_EQ(std::get<1>(room.getUserList()[0]), "John");
+}*/
